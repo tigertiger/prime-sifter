624756752d5a4ee89032d52de4e4f56a5c0a9322
Melissa <melissa.scmi@gmail.com>
2021-09-22 09:25:21 -0700

    add initial boilerplate code
    Co-authored-by: Roman Kolivashko <rkolivashko@gmail.com>

---
+++ PrimeSifter.Tests/ModelTests/NumberTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimeSifter;
+
+namespace PrimeSifter.Tests
+{
+  [TestClass]
+  public class NumbersTests
+  {
+    // Test methods go here
+  }
+}
